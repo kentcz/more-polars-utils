7983cb1670ef288eea64b28a7c6b7b4a7f2ad209
Kent Czechowski <kentcz@hotmail.com>
2024-09-13 14:23:46 -0500

    Fix formatting error

--- more_polars_utils/__init__.py
+++ more_polars_utils/__init__.py
@@ -1,4 +1,4 @@
-import more_polars_utils.common.dataframe_ext # noqa: F401
+import more_polars_utils.common.dataframe_ext  # noqa: F401
 from more_polars_utils.common.io import read_parquet, parquet_file_size
 from more_polars_utils.common.dataframe_assets import ASSET_MANAGER, ACTIVE_PROJECT
 
