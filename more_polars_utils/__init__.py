64f12ddb9602598f2ae1b9ec184f8e493ec8a271
Kent Czechowski <kentcz@hotmail.com>
2024-09-13 14:04:56 -0500

    Add DataFrame Assets

---
+++ more_polars_utils/__init__.py
@@ -0,0 +1,3 @@
+import more_polars_utils.common.dataframe_ext
+from more_polars_utils.common.io import read_parquet, parquet_file_size
+from more_polars_utils.common.dataframe_assets import ASSET_MANAGER, ACTIVE_PROJECT
